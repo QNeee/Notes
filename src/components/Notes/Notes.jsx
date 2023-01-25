64b1a7a46634735c7f60d3ca1b151f75fa839f75
QNeee <vovasagan7@gmail.com>
2023-01-25 14:35:15 +0300

    styles

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -4,7 +4,6 @@ import { Context } from 'components/context';
 import { useContext } from 'react';
 export const Notes = () => {
     const { node, onClickDelete, onClickRedactor } = useContext(Context);
-    console.log(node);
     return <Container>
         {node.length > 0 ? node.map(item => <div key={item.id}><h1>{item.name}</h1><h3>{item.text}</h3><Button variant="contained" onClick={(e) => onClickDelete(item.id)} type="button">Delete</Button><Button variant="contained" onClick={() => onClickRedactor(item)} type="button">Redactor</Button></div>) : <h1>Choose Note</h1>}
     </Container>
